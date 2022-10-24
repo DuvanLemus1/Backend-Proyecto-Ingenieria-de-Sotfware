269f21c91140ddfedeea19f272a9eb80eb645bfb
LSuazo1 <57919412+LSuazo1@users.noreply.github.com>
2022-10-24 11:20:44 -0600

    Creado documento de config bd

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const {Sequelize}=require('sequelize');
+ 
+    const db = new Sequelize('nombrebase','root','contrasenia del gestor',{
+        host: 'localhost',
+        port:'3306',
+        dialect:'mysql',
+        define:{
+            timestamps:false
+        },
+        pool:{
+            max:5,
+            min:0,
+            acquire:30000,
+            idle:10000
+        },
+        operatorAliases:false
+    
+    });
+module.exports ={
+    db
+};
